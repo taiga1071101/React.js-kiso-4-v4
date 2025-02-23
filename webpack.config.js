26a7b36d3699430787ea52f178678a5f8dbe5253
taiga1071101 <taigatukimi@gmail.com>
2025-02-23 12:19:15 +0900

    webpackとbabel設定

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import webpackNodeExternals from 'webpack-node-externals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  // バンドルモードを指定
+  mode: "development",
+  target: 'node',
+  entry: './src/server/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    chunkFormat: "module",
+    library: {
+      type: 'module',  // モジュール形式でライブラリを出力
+    },
+    module: true,  // 出力をESMに設定
+  },
+  experiments: {
+    outputModule: true,  // モジュールとして出力
+  },
+  externals: [
+    webpackNodeExternals({
+      importType: 'module', // `import express from 'express'` の形でバンドルする
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/, // どの拡張子にBabel-loaderを適用させるか
+        exclude: /node_modules/, // 除外するファイルやディレクトリ
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                '@babel/preset-env',  // 最新のJavaScriptを変換
+                '@babel/preset-react',  // JSXを変換
+                '@babel/preset-typescript',
+              ],
+            },
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    fallback: {
+      'url': false,  // 無効化（Webpack 5 から Node.js のコアモジュールのポリフィルが自動的に含まれなくなった）
+      'path': false,
+      'fs': false,
+      "crypto": false,
+      "stream": false,
+      "buffer": false,
+      "http": false,
+      "https": false,
+      'querystring': false,
+      'net': false,
+      'zlib': false,
+    },
+  },
+};
